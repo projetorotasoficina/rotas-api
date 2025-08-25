03bee9ad13481b102950a9fe883c32df015c4a6b
Luiz <passosl@alunos.utfpr.edu.br>
2025-08-24 22:34:43 -0300

    chore: commit inicial do projeto Spring Boot

---
+++ src/main/java/utfpr/edu/br/coleta/ColetaApplication.java
@@ -0,0 +1,13 @@
+package utfpr.edu.br.coleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColetaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utfpr/edu/br/coleta/ColetaApplicationTests.java
@@ -0,0 +1,13 @@
+package utfpr.edu.br.coleta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
