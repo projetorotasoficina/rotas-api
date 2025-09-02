585acd66c3faf61f057c09c9df768f350e5dc8bd
Luiz <passosl@alunos.utfpr.edu.br>
2025-09-02 01:41:40 -0300

    feat(usuario): implementar CRUD de usuário sem senha e corrigir configuração de testes com H2/Flyway

---
+++ src/main/java/utfpr/edu/br/coleta/usuario/role/Role.java
@@ -0,0 +1,10 @@
+package utfpr.edu.br.coleta.usuario.role;
+
+/**
+ * Roles disponíveis no sistema para controle de permissões.
+ */
+public enum Role {
+    ROLE_SUPER_ADMIN,
+    ROLE_ADMIN_CONSULTA,
+    ROLE_CLIENTE
+}
