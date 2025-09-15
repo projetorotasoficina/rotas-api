0697d246baba431f198970c95e1ce01528dd687e
pedrohsauthier <phsauthier@yahoo.com>
2025-09-15 20:10:13 -0300

    Rota.java finalizado falta apenas testar

--- src/main/java/utfpr/edu/br/coleta/rota/Rota.java
+++ src/main/java/utfpr/edu/br/coleta/rota/Rota.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import utfpr.edu.br.coleta.generics.BaseEntity;
+import utfpr.edu.br.coleta.tipocoleta.TipoColeta;
+import utfpr.edu.br.coleta.tiporesiduo.TipoResiduo;
 
 /**
  * Entidade que representa uma Rota no sistema de coleta.
@@ -22,4 +24,26 @@ import utfpr.edu.br.coleta.generics.BaseEntity;
 @NoArgsConstructor
 public class Rota extends BaseEntity {
 
+    /** Nome/Descrição do tipo de coleta. */
+    @NotBlank(message = "Um nome é obrigatório.")
+    @Column(nullable = false)
+    private String nome;
+
+    @NotNull(message = "O campo ativo é obrigatório.")
+    @Column(nullable = false)
+    private Boolean ativo;
+
+    @Size(max = 400, message = "Observações podem ter no máximo 400 caracteres.")
+    @Column(length = 400)
+    private String observacoes;
+
+    @NotNull(message = "O tipo de resíduo é obrigatório.")
+    @ManyToOne
+    @JoinColumn(name = "tipo_residuo_id", nullable = false)
+    private TipoResiduo tipoResiduo;
+
+    @NotNull(message = "O tipo de coleta é obrigatório.")
+    @ManyToOne
+    @JoinColumn(name = "tipo_coleta_id", nullable = false)
+    private TipoColeta tipoColeta;
 }
