4b3a79bdf6c1ebcab29b0d3baf93893f44261443
Luiz <passosl@alunos.utfpr.edu.br>
2025-10-15 22:55:52 -0300

    permite qualquer email

--- src/main/java/utfpr/edu/br/coleta/auth/dto/SolicitacaoCodigoOTPRequestDTO.java
+++ src/main/java/utfpr/edu/br/coleta/auth/dto/SolicitacaoCodigoOTPRequestDTO.java
@@ -8,8 +8,7 @@ import lombok.Data;
 @Data
 @Builder
 public class SolicitacaoCodigoOTPRequestDTO {
-  @Email(
-      regexp = "^[a-zA-Z0-9._%+-]+@(alunos\\.utfpr\\.edu\\.br|utfpr\\.edu\\.br)$",
-      message = "E-mail deve ser @utfpr.edu.br ou @alunos.utfpr.edu.br")
-  @NotEmpty private String email;
+  @Email(message = "E-mail deve ser válido")
+  @NotEmpty
+  private String email;
 }
