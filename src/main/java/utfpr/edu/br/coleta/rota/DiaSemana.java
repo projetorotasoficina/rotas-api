e0da567626aef13b443124941e4d3f35d420d503
Luiz <passosl@alunos.utfpr.edu.br>
2025-10-22 00:21:26 -0300

    adcionar frequencia rota

---
+++ src/main/java/utfpr/edu/br/coleta/rota/DiaSemana.java
@@ -0,0 +1,17 @@
+package utfpr.edu.br.coleta.rota;
+
+/**
+ * Enum que representa os dias da semana para frequência de rotas.
+ *
+ * Autor: Sistema
+ */
+public enum DiaSemana {
+    DOMINGO,
+    SEGUNDA,
+    TERCA,
+    QUARTA,
+    QUINTA,
+    SEXTA,
+    SABADO
+}
+
